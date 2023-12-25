ab710bff0532979af0a068772e869c889a9859ef
Niachal Khanal <khanalnischal2075@gmail.com>
2023-12-25 22:56:07 +0545

    feat:search bar

--- client/src/pages/TestingComponent.jsx
+++ client/src/pages/TestingComponent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import InputField from "../components/InputField";
+import SearchBar from "../components/SearchBar";
 
 export default function TestingComponent() {
     return (
         <div>
-        <InputField />
+        {/* <InputField /> */}
+        <SearchBar />
         </div>
     );
     }
